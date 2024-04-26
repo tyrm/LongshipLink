040ed272962bcbec323af128471e9aa389924a25
Tyr Mactire <tyr@pettingzoo.co>
2024-04-26 15:37:39 -0700

    get query params

--- cloudflare-auth/src/lib.rs
+++ cloudflare-auth/src/lib.rs
@@ -1,6 +1,40 @@
 use worker::*;
+use url::Url;
+use serde_json::json;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, ctx: Context) -> Result<Response> {
-    Response::ok("Hello, World!")
+    let url = Url::parse(&req.url().unwrap().to_string())?;
+    let path = url.path();
+
+    match path {
+        "/api/v1/auth/user" => {
+            let mut sid = None;
+            let mut secret = None;
+            let mut uid = None;
+
+            for (key, value) in url.query_pairs() {
+                match key.as_ref() {
+                    "sid" => sid = Some(value.into_owned()),
+                    "secret" => secret = Some(value.into_owned()),
+                    "uid" => uid = Some(value.into_owned()),
+                    _ => {}
+                }
+            }
+
+            // Create a JSON object
+            let params = json!({
+                "sid": sid,
+                "secret": secret,
+                "uid": uid
+            });
+
+            // Convert the JSON object to a string
+            let params_string = params.to_string();
+
+            // Return the JSON string as the response
+            Response::ok(params_string)
+        },
+        _ => Response::error("Not Found", 404),
+    }
 }
