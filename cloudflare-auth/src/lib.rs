5984ec73a98946d821882829f9693ae81d39a3fb
Tyr Mactire <tyr@pettingzoo.co>
2024-04-29 20:15:51 -0700

    progress

--- cloudflare-auth/src/lib.rs
+++ cloudflare-auth/src/lib.rs
@@ -1,6 +1,6 @@
 use worker::*;
-use url::Url;
 use serde_json::json;
+use serde::Deserialize;
 
 #[event(fetch)]
 async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
@@ -11,43 +11,44 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
         .await
 }
 
+#[derive(Deserialize)]
+struct AuthQuery {
+    sid: Option<String>,
+    secret: Option<String>,
+    uid: Option<String>,
+}
+
 async fn authenticate_user(req: Request, ctx: RouteContext<()>) -> Result<Response> {
-    let url = Url::parse(&req.url().unwrap().to_string())?;
     let kv = ctx.env.kv("ll-dev")?;
 
-    let mut sid = None;
-    let mut secret = None;
-    let mut uid = None;
-
-    for (key, value) in url.query_pairs() {
-        match key.as_ref() {
-            "sid" => sid = Some(value.into_owned()),
-            "secret" => secret = Some(value.into_owned()),
-            "uid" => uid = Some(value.into_owned()),
-            _ => {}
-        }
-    }
+    let query: AuthQuery = req.query()?;
 
     // Return 400 error if any of the required parameters are missing
-    if sid.is_none() || secret.is_none() || uid.is_none() {
+    if query.sid.is_none() || query.secret.is_none() || query.uid.is_none() {
         return Response::error("Missing required parameter", 400);
     }
 
-    let sid_string = match sid {
+    let sid = match query.sid {
         Some(sid) => sid,
         None => {
             return Response::error("Missing required parameter", 400);
         }
     };
-    let secret_string = match secret {
+    let secret = match query.secret {
         Some(secret) => secret,
+        None => {
+            return Response::error("Missing required parameter", 400);
+        }
+    };
+    let uid = match query.uid {
+        Some(uid) => uid,
         None => {
             return Response::error("Missing required parameter", 400);
         }
     };
 
     // read server secret by sid
-    let secret_key = format!("server.{}.secret", sid_string);
+    let secret_key = format!("server.{}.secret", sid);
     let kv_secret_result = kv.get(&secret_key).text().await;
     let kv_secret = match kv_secret_result {
         Ok(Some(secret)) => secret,
@@ -63,15 +64,15 @@ async fn authenticate_user(req: Request, ctx: RouteContext<()>) -> Result<Respon
     console_debug!("kv_key: {}, kv_secret: {}", secret_key, kv_secret);
 
     // Compare the secret from the request with the secret from KV
-    if secret_string != kv_secret {
-        console_debug!("Secret mismatch: {} != {}", secret_string, kv_secret);
+    if secret != kv_secret {
+        console_debug!("Secret mismatch: {} != {}", secret, kv_secret);
         return Response::error("Unauthorized", 401);
     };
 
     // Create a JSON object
     let params = json!({
-        "sid": sid_string,
-        "secret": secret_string,
+        "sid": sid,
+        "secret": secret,
         "uid": uid,
     });
     Response::ok(params.to_string())
